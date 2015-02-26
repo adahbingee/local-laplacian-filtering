4a824f4e9c283ce5e67412da746c8244e14ca108
adahbingee <adahbingee@bbcfdc06-5fde-ed20-9183-b36cf6d196d6>
2015-02-26 03:46:54 +0000

    it works! to do : read back from GPU

--- main.js
+++ main.js
@@ -26,6 +26,9 @@ kernelUp.data    = [ 1/64,  4/64,  6/64,  4/64, 1/64,
 					 6/64, 24/64, 36/64, 24/64, 6/64,
 					 4/64, 16/64, 24/64, 16/64, 4/64,
 					 1/64,  4/64,  6/64,  4/64, 1/64];
+		
+// opengl inatance		
+var gl;
 				
 $( document ).ready(function(){
 	// load parameters
@@ -44,6 +47,107 @@ $( document ).ready(function(){
 	img.onload = onImageLoad;
 	img.src    = imagepath;
 });
+
+function onWebGL( img ) {
+        var canvas = document.getElementById("webgl");
+        canvas.width  = img.cols;
+        canvas.height = img.rows;
+        gl = getWebGLContext(canvas);
+        if ( !gl ) return;
+        
+        // setup GLSL program
+        var program = createProgramFromScripts(gl, ["2d-vertex-shader", "2d-fragment-shader"]);
+        gl.useProgram(program);
+        
+        // look up where the vertex data needs to go.
+        var positionLocation = gl.getAttribLocation(program, "a_position");
+        var texCoordLocation = gl.getAttribLocation(program, "a_texCoord");
+        var kernelLocation   = gl.getUniformLocation(program, "u_kernel[0]");
+        
+        // provide texture coordinates for the rectangle.
+        var texCoordBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+          0.0,  0.0,
+          1.0,  0.0,
+          0.0,  1.0,
+          0.0,  1.0,
+          1.0,  0.0,
+          1.0,  1.0]), gl.STATIC_DRAW);
+        gl.enableVertexAttribArray(texCoordLocation);
+        gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
+        
+        // Create a texture.
+        var texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        
+        // Set the parameters so we can render any size image.
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+		
+		// set float texture support
+		gl.getExtension("OES_texture_float");
+		
+		// transform image data to float array
+		var data = new Float32Array(img.data.length);
+		for (var i = 0; i < img.data.length; ++i) {
+			data[i] = img.data[i];
+		}
+		
+		var format;
+		if (img.channel == 1) {
+			format = gl.LUMINANCE;
+		} else if (img.channel == 4) {
+			format = gl.RGBA;
+		} else if (img.channel == 3) {
+			format = gl.RGB;
+		}
+        
+        // Upload the image into the texture.
+		gl.texImage2D(gl.TEXTURE_2D, 0, format, img.cols, img.rows, 0, format, gl.FLOAT, data);
+
+        // lookup uniforms
+        var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+        var textureSizeLocation = gl.getUniformLocation(program, "u_textureSize");
+        var kernelLocation = gl.getUniformLocation(program, "u_kernel[0]");
+
+        // set the resolution
+        gl.uniform2f(resolutionLocation, canvas.width, canvas.height);
+        
+        // set the size of the image
+		gl.uniform2f(textureSizeLocation, img.cols, img.rows);
+
+        // Create a buffer for the position of the rectangle corners.
+        var buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.enableVertexAttribArray(positionLocation);
+        gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+        
+        // set kernel
+        gl.uniform1fv(kernelLocation, kernelDown.data);
+
+        // Set a rectangle the same size as the image.
+        setRectangle(gl, 0, 0, img.cols, img.rows);
+
+        // Draw the rectangle.
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+}
+
+function setRectangle(gl, x, y, width, height) {
+  var x1 = x;
+  var x2 = x + width;
+  var y1 = y;
+  var y2 = y + height;
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+     x1, y1,
+     x2, y1,
+     x1, y2,
+     x1, y2,
+     x2, y1,
+     x2, y2]), gl.STATIC_DRAW);
+}
 
 function onImageLoad(){
 	// get image parameters
@@ -67,6 +171,12 @@ function onImageLoad(){
 	for (var i = 0; i < imgGray.data.length; ++i) {
 		imgGray.data[i] = Math.log( imgGray.data[i] + 1.0 );
 	}
+	
+	imgGray.normalize(0, 255);
+	
+	onWebGL( imgGray );
+	
+	return;
 
 	// local laplacian pyramid using fast approach (???)
 	var means = getMeans(imgGray, maxPyramidNum);
